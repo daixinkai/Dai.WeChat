77ef0e9ffbf504aa32ba3f2db70b9d1aa6415e33
pokemon <dxk1211@hotmail.com>
2016-12-01 15:23:09 +0800

    create test website

---
+++ Dai.WeChat/Dai.WeChat.TestWeb/App_Start/Register.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Dai.WeChat.TestWeb
+{
+    class Register : IRegister
+    {
+        void IRegister.Register()
+        {
+
+        }
+
+        [PostApplicationStart]
+        static void ApplicationStart()
+        {
+            AspNetMvcConfig.RemoveWebFormViewEngine();
+
+            //AreaRegistration.RegisterAllAreas();
+
+
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
