dbdde28dc11c38d29ccbdc3913143d416919c074
pokemon <dxk1211@hotmail.com>
2016-12-01 15:12:20 +0800

    1

---
+++ Dai.WeChat/Dai.WeChat.Core/WeChatHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dai.WeChat
+{
+    public static class WeChatHelper
+    {
+        #region EchoPass
+
+        /// <summary>
+        /// 告知微信服务器，已通过验证
+        /// </summary>
+        public static void EchoPass(this HttpContext context, string echostr)
+        {
+            context.Response.Write(echostr);
+            context.Response.Flush();
+        }
+
+        /// <summary>
+        /// 告知微信服务器，已通过验证
+        /// </summary>
+        public static void EchoPass(this HttpContext context)
+        {
+            context.EchoPass(context.Request["echostr"]);
+        }
+
+        /// <summary>
+        /// 告知微信服务器，已通过验证
+        /// </summary>
+        public static void EchoPass(string echostr)
+        {
+            HttpContext.Current.EchoPass(echostr);
+        }
+
+        /// <summary>
+        /// 告知微信服务器，已通过验证
+        /// </summary>
+        public static void EchoPass()
+        {
+            EchoPass(HttpContext.Current.Request["echostr"]);
+        }
+
+        #endregion
+    }
+}
